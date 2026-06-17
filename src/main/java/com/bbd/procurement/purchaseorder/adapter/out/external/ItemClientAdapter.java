83a9b8f49ba9b471215c8894d90d97b0c787b81f
inwoohub <dlsdndls@naver.com>
2026-06-17 18:21:34 +0900

    [Fix] : getItem 메서드 로그 확인

--- src/main/java/com/bbd/procurement/purchaseorder/adapter/out/external/ItemClientAdapter.java
+++ src/main/java/com/bbd/procurement/purchaseorder/adapter/out/external/ItemClientAdapter.java
@@ -5,9 +5,11 @@ import com.bbd.procurement.global.error.ErrorCode;
 import com.bbd.procurement.purchaseorder.application.port.out.LoadItemPort;
 import com.bbd.procurement.purchaseorder.application.port.out.result.ItemResult;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ItemClientAdapter implements LoadItemPort {
@@ -26,6 +28,7 @@ public class ItemClientAdapter implements LoadItemPort {
         } catch (HttpClientErrorException.NotFound e) {
             throw new ApiException(ErrorCode.ITEM_NOT_FOUND);
         } catch (Exception e) {
+            log.info( "⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️ {} ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️", e.getMessage());
             throw new ApiException(ErrorCode.ITEM_SERVICE_ERROR);
         }
     }
